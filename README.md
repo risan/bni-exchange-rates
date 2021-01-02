c5f957bdec1548747eeae7219803229e595897d6
risan <risanbagja@gmail.com>
2021-01-03 00:04:50 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# BNI Exchange Rates API
+
+API to get Indonesian Rupiah exchange rates from BNI. All exchange rates data are scraped automatically from [BNI Forex Information](https://bni.co.id/en-us/home/forexinformation) page.
+
+## Installation
+
+You'll need a [Vercel](https://vercel.com/home) account and [Vercel CLI](https://vercel.com/download) to spin up this API.
+
+```bash
+# Clone the repository
+$ git clone git@github.com:risan/bni-exchange-rates.git
+
+# Install dependencies
+$ npm install
+
+# Spin up vercel development
+$ vercel dev
+```
---
+++ api/index.js
@@ -0,0 +1,77 @@
+const fs = require('fs');
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const CACHE_PAGE = 'cache/page.html';
+
+const getPage = async (clearCache = false) => {
+  if (!clearCache && fs.existsSync(CACHE_PAGE)) {
+    return {
+      fromCache: true,
+      page: fs.readFileSync(CACHE_PAGE),
+    };
+  }
+
+  const response = await axios.request({
+    method: 'get',
+    url: 'https://bni.co.id/en-us/home/forexinformation',
+    headers: {
+      Accept: 'text/html',
+      'Accept-Encoding': 'gzip, deflate, br',
+      Pragma: 'no-cache',
+      'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0',
+    },
+  });
+
+  fs.writeFileSync(CACHE_PAGE, response.data);
+
+  return {
+    fromCache: false,
+    page: response.data,
+  };
+};
+
+const parseNumber = val => {
+  return parseFloat(val.replace(/[,]/g, ''));
+};
+
+const parseSection = ($, id) => {
+  const items = $(`${id} table tbody tr`).map((i, row) => {
+    const cols = $('td', row);
+
+    return {
+      currency: cols.eq(0).text(),
+      buy: parseNumber(cols.eq(2).text()),
+      sell: parseNumber(cols.eq(1).text()),
+    };
+  }).get();
+
+  const dateInfo = $(`${id} .date-infoView span`).text();
+
+  const matches = dateInfo.match(/(\d{2}\/\d{2}\/\d{4} \d{2}:\d{2}:\d{2}).+([+-]\d{2}:\d{2})/);
+
+  if (matches === null) {
+    return { items };
+  }
+
+  const date = new Date(`${matches[1]} ${matches[2]}`);
+
+  return { last_update: date.toISOString(), idr_rate: items };
+};
+
+module.exports = async (req, res) => {
+  try {
+    const { fromCache, page } = await getPage(['1', 'true'].includes(req.query.nocache));
+    const $ = cheerio.load(page);
+
+    return res.json({
+      data: {
+        from_cache: fromCache,
+        bank_notes: parseSection($, '#dnn_ctr3509_BNIValasInfoView_divSpecial'),
+        counter: parseSection($, '#dnn_ctr3509_BNIValasInfoView_divBankNotes'),
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
